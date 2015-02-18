78d146849aa2c144f6dca8601dfaa818f4b09c09
Jay <jhd@fastmail.net>
2015-02-18 11:55:15 -0500

    add sign-up

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -7,5 +7,24 @@ get '/login' do
 end
 
 get '/signup' do
+
+  erb :'/auth/signup'
   
+end
+
+post '/signup' do
+  puts params[:user]
+  new_user = User.create(params[:user])
+  unless new_user.id
+    parse_errors(new_user.errors.messages)
+    redirect '/login'
+  end
+
+  redirect '/'
+
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
 end
